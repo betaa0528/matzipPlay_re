5c56f40fc1ceedc2785128b10e528cb91eeda8cc
hyeri <hrpark9504@gmail.com>
2023-09-07 14:40:39 +0900

    깃 연결 확인

--- src/main/java/com/restaurantProject/famousrestaurant/controller/mypageController.java
+++ src/main/java/com/restaurantProject/famousrestaurant/controller/mypageController.java
@@ -3,16 +3,24 @@ package com.restaurantProject.famousrestaurant.controller;
 import com.restaurantProject.famousrestaurant.repository.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
-public class testController {
+public class mypageController {
     private final RestaurantRepository restaurantRepository;
+    @GetMapping("/mypage")
+    public String getMypage(Model model){
+        //아이디 확인: session은 db두 개 다 확인필요
+        String id = "hr1234";
 
-    @GetMapping("/")
-    public String test() {
-        restaurantRepository.findAll().forEach(System.out::println);
-        return "index";
+        //찜목록 가져오기
+
+        //리뷰목록 가져오기
+
+        model.addAttribute("ss",id);
+
+        return "mypage";
     }
 }
