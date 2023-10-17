4a6b88c653a0009fff54dccc3a4102592c1e3c74
yun <lolo1123l@naver.com>
2023-10-17 21:24:24 +0900

    2023-10-17 배포 ver0.9999

--- src/main/java/com/restaurantProject/famousrestaurant/util/NaverLoginApi.java
+++ src/main/java/com/restaurantProject/famousrestaurant/util/NaverLoginApi.java
@@ -23,7 +23,7 @@ public class NaverLoginApi {
     //state: 애플리케이션이 생성한 상태 토큰
     private final static String CLIENT_ID = "FqZT48fbncyE6lF33fIA";
     private final static String CLIENT_SECRET = "GOhbu902pM";
-    private final static String REDIRECT_URI = "http://localhost:8080/callback";
+    private final static String REDIRECT_URI = "http://restaurant-env.eba-nrkpacdt.ap-northeast-2.elasticbeanstalk.com/callback";
     private final static String SESSION_STATE = "oauth_state";
     /* 프로필 조회 API URL */
     private final static String PROFILE_API_URL = "https://openapi.naver.com/v1/nid/me";
