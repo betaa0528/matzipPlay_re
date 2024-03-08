3a94e5ee4e7bb358ada994c9d34df7051dfb7df8
minju <betaa0528@gmail.com>
2024-03-08 20:26:49 +0900

    #8 - 테스트 주석처리
    사용하지 않는 테스트클래스들의 주석처리를 하였다.

---
+++ src/test/java/com/restaurantProject/famousrestaurant/repository/ReviewRepositoryTest.java
@@ -0,0 +1,71 @@
+package com.restaurantProject.famousrestaurant.repository;
+
+import com.restaurantProject.famousrestaurant.dto.Review;
+import com.restaurantProject.famousrestaurant.entity.RestaurantEntity;
+import com.restaurantProject.famousrestaurant.entity.ReviewEntity;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+@SpringBootTest
+public class ReviewRepositoryTest {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    @Test
+    public void repositoryTest() {
+
+//        List<RestaurantEntity> restaurantEntities = restaurantRepository.findAll();
+//
+//        ReviewEntity review1 = new ReviewEntity();
+//        review1.setId(1L);
+//        review1.setMemberId("user1");
+//        review1.setRestaurantEntity(restaurantEntities.get(0));
+//        System.out.println(review1);
+        LocalDateTime now = LocalDateTime.now();
+        String format = now.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+//        LocalDateTime nwer = now.toLocalDate().;
+//        System.out.println(nwer);
+
+    }
+
+    @DisplayName("리뷰에 버튼들의 숫자의 갯수를 확인한다")
+    @Test
+    void givenTestData_whenCountRecommend_thenWorkFine () {
+        Optional<RestaurantEntity> restaurantEntity = restaurantRepository.findById(17L);
+        System.out.println(restaurantEntity);
+//
+//        Page<ReviewEntity> reviewEntities = reviewRepository.findAllByRestaurantEntityOrderByIdDesc(restaurantEntity.get());
+//        System.out.println(reviewEntities.getTotalElements());
+
+//        HashMap<String, Integer> hashMap = new HashMap<>();
+//        for(ReviewEntity review : reviewEntities) {
+////            System.out.println("review_id : " + review.getId() + ", member_id" + review.getMemberId() +
+////                    ", recommend " + review.getRecommendValues());
+//            String[] split = review.getRecommendValues().split(",");
+//            for(String sp : split) {
+//                hashMap.put(sp, hashMap.getOrDefault(sp, 0) + 1);
+//            }
+//        }
+//
+//        System.out.println(hashMap);
+//        Comparator<? super Map.Entry<String, Integer>> Comparator = new Comparator<Map.Entry<String, Integer>>() {
+//            @Override
+//            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+//                return o1.getValue().compareTo(o2.getValue());
+//            }
+//        };
+//        Map.Entry<String , Integer> maxEntry = Collections.max(hashMap.entrySet(), Comparator);
+//        System.out.println(maxEntry.getKey() + ", " +maxEntry.getValue());
+
+    }
+}
--- src/test/java/com/restaurantProject/famousrestaurant/service/ReviewServiceTest.java
+++ src/test/java/com/restaurantProject/famousrestaurant/service/ReviewServiceTest.java
@@ -1,22 +1,53 @@
 package com.restaurantProject.famousrestaurant.service;
 
+import com.restaurantProject.famousrestaurant.dto.Review;
 import com.restaurantProject.famousrestaurant.entity.ReviewEntity;
 import com.restaurantProject.famousrestaurant.repository.ReviewRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class ReviewServiceTest {
 
     @Autowired
     private ReviewRepository repository;
+    @Autowired
+    private ReviewService reviewService;
     @Test
     void recommendTest() {
         ReviewEntity all = repository.findById(1L).get();
         System.out.println(all);
+    }
+
+    @DisplayName("findByRestaurantId method test")
+    @Test
+    void givenRestaurantId_whenCountRecommend_thenFineWork() {
+//        List<Review> list = reviewService.findByRestaurantId(17L);
+//        System.out.println(Arrays.toString(list.get(0).getRecommendValues()));
+//        HashMap<String, Integer> hashMap = new HashMap<>();
+//        for(Review review : list) {
+//            String[] split = review.getRecommendValues();
+//            for(String sp : split) {
+//                hashMap.put(sp, hashMap.getOrDefault(sp, 0) + 1);
+//            }
+//        }
+//
+//        Comparator<? super Map.Entry<String, Integer>> Comparator = new Comparator<Map.Entry<String, Integer>>() {
+//            @Override
+//            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+//                return o1.getValue().compareTo(o2.getValue());
+//            }
+//        };
+//        Map.Entry<String , Integer> maxEntry = Collections.max(hashMap.entrySet(), Comparator);
+////        System.out.println(maxEntry.getKey() + ", " +maxEntry.getValue());
+//
+//        assertEquals("11", maxEntry.getKey());
     }
 }
