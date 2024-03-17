337447df2281b2b8237a14470d6780857ef274af
minju <betaa0528@gmail.com>
2024-03-17 20:19:20 +0900

    #11 - myPage 서비스 로직 수정
    기존 엔티티를 사용해서 값을 넘겨주는 부분들에 대해서는 dto로 변환하여 넘겨주고 로직을 단순화 할 수 있는건 단순화 하였다.

--- src/main/java/com/restaurantProject/famousrestaurant/service/MyPageService.java
+++ src/main/java/com/restaurantProject/famousrestaurant/service/MyPageService.java
@@ -3,18 +3,28 @@ package com.restaurantProject.famousrestaurant.service;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.restaurantProject.famousrestaurant.dto.Restaurant;
+import com.restaurantProject.famousrestaurant.dto.Review;
+import com.restaurantProject.famousrestaurant.dto.WishList;
+import com.restaurantProject.famousrestaurant.entity.MemberEntity;
 import com.restaurantProject.famousrestaurant.entity.ReviewEntity;
 import com.restaurantProject.famousrestaurant.entity.WishListEntity;
 import com.restaurantProject.famousrestaurant.repository.MemberRepository;
+import com.restaurantProject.famousrestaurant.repository.RestaurantRepository;
 import com.restaurantProject.famousrestaurant.repository.ReviewRepository;
 import com.restaurantProject.famousrestaurant.repository.WishListRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -24,10 +34,14 @@ public class MyPageService {
     private final WishListRepository wishListRepository;
     private final ReviewRepository reviewRepository;
     private final RestaurantService restaurantService;
+    private final ReviewService reviewService;
     private final AmazonS3 amazonS3;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
+
+    private final String profilePath = "C:/profile/";
+    private final RestaurantRepository restaurantRepository;
 
     private String upload(MultipartFile file) throws IOException {
         if (!file.isEmpty()) {
@@ -37,11 +51,14 @@ public class MyPageService {
             String[] uuids = uuid.toString().split("-");
             String uniqueName = uuids[0];
             String fileName = uniqueName + fileExtension;
+            String savePath = profilePath + fileName;
+            file.transferTo(new File(savePath));
 
-            ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentLength(file.getSize());
-            metadata.setContentType(file.getContentType());
-            amazonS3.putObject(bucket+"/profile",fileName,file.getInputStream(),metadata);
+
+//            ObjectMetadata metadata = new ObjectMetadata();
+//            metadata.setContentLength(file.getSize());
+//            metadata.setContentType(file.getContentType());
+//            amazonS3.putObject(bucket + "/profile", fileName, file.getInputStream(), metadata);
             return fileName;
 
         } else {
@@ -50,35 +67,23 @@ public class MyPageService {
     }
 
     public void delete(String fileName) {
-        amazonS3.deleteObject(bucket, fileName);
+//        amazonS3.deleteObject(bucket, fileName);
+        System.out.println("삭제 완");
     }
 
-//    public boolean profileUpload(MultipartFile file, String memberId) throws IOException {
-//        Optional<MemberEntity> memOptional = memberRepository.findByMemberId(memberId);
-//        if (memOptional.isPresent()) {
-//            MemberEntity mem = memOptional.get();
-//            if (mem.getMemberProfile().equals("default.jpeg")) {
-//                String profile = upload(file);
-//                mem.setMemberProfile(profile);
-//            } else {
-//                delete(mem.getMemberProfile());
-//                String profile = upload(file);
-//                mem.setMemberProfile(profile);
-//            }
-//            memberRepository.save(mem);
-//            return true;
-//        }else {
-//            return false;
-//        }
-//    }
+    public boolean profileUpload(MultipartFile file, String memberId) throws IOException {
+        MemberEntity member = memberRepository.getByMemberId(memberId);
+        if (!member.getMemberProfile().equals("default.jpeg")) {
+            delete(member.getMemberProfile());
+        }
+        String profile = upload(file);
+        member.setMemberProfile(profile);
+        memberRepository.save(member);
+        return true;
+    }
 
-    public boolean reviewDelete(Long id) {
-        Optional<ReviewEntity> r = reviewRepository.findById(id);
-        if (r.isPresent()) { // Optional에서 엔티티 추출
-            ReviewEntity reviewEntity = r.get();
-            reviewRepository.delete(reviewEntity); // 엔티티를 사용하여 삭제
-            return true;
-        } else return false;
+    public void reviewDelete(Long id) {
+        reviewRepository.deleteById(id);
     }
 
     public boolean wishListDelete(Long id) {
@@ -87,25 +92,31 @@ public class MyPageService {
             WishListEntity wishListEntity = w.get();
             wishListRepository.delete(wishListEntity); // 엔티티를 사용하여 삭제
             return true;
-        }return false;
+        }
+        return false;
     }
 
-    public List<ReviewEntity> getMyReview(String memberId) { return reviewRepository.findByMemberId(memberId);}
+    public List<Review> getMyReview(String memberId) {
+        List<ReviewEntity> reviewEntities = reviewRepository.findAllByMemberId(memberId);
+        return reviewEntities.stream().map(Review::from).collect(Collectors.toList());
+    }
 
-    public List<WishListEntity> getMyWish(String memberId){ return wishListRepository.findByMemberWishId(memberId); }
+    public List<WishList> getMyWish(String memberId) {
+        List<WishListEntity> entityList = wishListRepository.findByMemberWishId(memberId);
+        for(WishListEntity entity : entityList) {
+            entity.setRestaurantEntity(restaurantRepository.findById(entity.getRestaurantId()).get());
+        }
+        return entityList.stream().map(WishList::from).collect(Collectors.toList());
+    }
 
-    public List<String> getMyWishImage(List<WishListEntity> wishList){
+    public List<String> getMyWishImage(List<WishList> wishList) {
         List<String> image = new ArrayList<>();
-        for (WishListEntity w : wishList) {
+        for (WishList w : wishList) {
             Restaurant restaurant = restaurantService.findById(w.getRestaurantId());
             String query = restaurant.getRestaurantName();
             image.add(restaurantService.imgSearch(query));
         }
         return image;
     }
-
-//    public Optional<MemberEntity> findByMemberId(String id){
-//       return memberRepository.findByMemberId(id);
-//    }
 
 }
