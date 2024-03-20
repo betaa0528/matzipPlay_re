25ae4b439dd2a16f7563fabbd15595ce11d2ac83
minju <betaa0528@gmail.com>
2024-03-20 20:01:18 +0900

    #17 - 타임리프 decoupled logic 설정
    java code 로 thymeleaf 3 decoupled logic 을
    사용자 application property 로 제어할 수 있게
    설정 추가

--- src/main/java/com/restaurantProject/famousrestaurant/FamousrestaurantApplication.java
+++ src/main/java/com/restaurantProject/famousrestaurant/FamousrestaurantApplication.java
@@ -3,9 +3,11 @@ package com.restaurantProject.famousrestaurant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class FamousrestaurantApplication {
 	public static void main(String[] args) {
