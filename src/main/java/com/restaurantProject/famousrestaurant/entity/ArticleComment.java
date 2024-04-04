e50cb7cb13e52f5aaa93e8bfe05e067617edccc6
minju <betaa0528@gmail.com>
2024-04-04 20:46:34 +0900

    #19 - 댓글 관련 Entity 추가
    Article의 댓글을 위해 ArticleComment entity를 추가해 주었다.

---
+++ src/main/java/com/restaurantProject/famousrestaurant/entity/ArticleComment.java
@@ -0,0 +1,40 @@
+package com.restaurantProject.famousrestaurant.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@ToString(callSuper = true)
+public class ArticleComment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @ManyToOne(optional = false)
+    private Article article;
+
+    @Setter
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "memberId")
+    private MemberEntity member;
+
+    @Setter
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    protected ArticleComment(){}
+
+    private ArticleComment(Article article, MemberEntity member, String content) {
+        this.article = article;
+        this.member = member;
+        this.content = content;
+    }
+
+
+}
