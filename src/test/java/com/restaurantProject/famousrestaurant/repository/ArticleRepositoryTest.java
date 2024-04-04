bd7e09a14e169ab3066ec08282a72b9beda096ed
minju <betaa0528@gmail.com>
2024-04-04 20:48:05 +0900

    #19 - ArticleRepositoryTest

---
+++ src/test/java/com/restaurantProject/famousrestaurant/repository/ArticleRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.restaurantProject.famousrestaurant.repository;
+
+import com.restaurantProject.famousrestaurant.dto.ArticleDto;
+import com.restaurantProject.famousrestaurant.dto.Member;
+import com.restaurantProject.famousrestaurant.entity.Article;
+import com.restaurantProject.famousrestaurant.entity.MemberEntity;
+import com.restaurantProject.famousrestaurant.entity.constant.ArticleType;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class ArticleRepositoryTest {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+    @Autowired
+    private MemberRepository memberRepository;
+
+
+    @Test
+    public void articleTest() {
+        MemberEntity member = memberRepository.findById(1L).get();
+        Member memberDto = Member.from(member);
+
+        ArticleDto dto = new ArticleDto("title1", "content1", memberDto, ArticleType.FREE_BOARD);
+//        System.out.println(dto);
+        Article article = Article.of(
+                member,
+                dto.getTitle(),
+                dto.getContent(),
+                dto.getArticleType());
+        articleRepository.save(article);
+        List<Article> all = articleRepository.findAll();
+        all.forEach(System.out::println);
+
+//        Article article = new Article() ;
+    }
+
+}
