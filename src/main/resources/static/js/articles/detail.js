6bf0eef02d23534f893308e8abb17bef3ddb4017
minju <betaa0528@gmail.com>
2024-04-10 20:16:16 +0900

    #19 - view 대대적인 수정
    게시글에 관련된 view들을 추가하고 css와 js도 작성뒤 articles 폴더를 만들어서 따로 관리함

---
+++ src/main/resources/static/js/articles/detail.js
@@ -0,0 +1,22 @@
+// 페이지 높이 조절 위한 변수
+const articleDiv = document.querySelector(".article-comments-div");
+// const infoWrap = document.querySelector(".restaurant-info-wrap");
+const main = document.querySelector('main');
+
+// 페이지 로드 시와 review-empty-div, review-present-div의 가시성 변경 시 높이를 조절합니다.
+function adjustSectionHeight() {
+    const articleDivHeight = articleDiv.clientHeight;
+    // const infoWrapHeight = infoWrap.clientHeight;
+    // const resultHeight = reviewDivHeight + infoWrapHeight;
+    main.style.height = `${articleDivHeight + 200}px`;
+}
+
+// function adjustReviewTextWidth() {
+//     const articleDivWidth = document.querySelector(".article-div");
+//     articleDivWidth.style.width = `100%`;
+// }
+//
+// window.addEventListener('load', adjustReviewTextWidth);
+// 페이지 로드 시와 review-empty-div, review-present-div의 가시성 변경 시 높이를 조절합니다.
+window.addEventListener('load', adjustSectionHeight);
+window.addEventListener('resize', adjustSectionHeight);
