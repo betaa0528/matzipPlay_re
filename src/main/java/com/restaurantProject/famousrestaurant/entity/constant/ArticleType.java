fc5541abbd06dfbef8c4763e55aafbca76daea88
minju <betaa0528@gmail.com>
2024-04-10 20:09:27 +0900

    #19 - 게시물 생성을 위한 Enum
    게시물 생성, 수정을 알려줄 FormStatus와 자유게시판과 신청게시판으로 나뉠수 있는 ArticleType Enum들을 작성하였다.

--- src/main/java/com/restaurantProject/famousrestaurant/entity/constant/ArticleType.java
+++ src/main/java/com/restaurantProject/famousrestaurant/entity/constant/ArticleType.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum ArticleType {
-    FREE_BOARD("자유게시판"),
-    REQUEST_BOARD("맛집신청게시판");
+    FREE("자유게시판"),
+    REQUEST("맛집신청게시판");
 
     private final String description;
 
